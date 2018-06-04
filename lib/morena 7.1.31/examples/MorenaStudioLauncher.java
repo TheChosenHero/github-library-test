eee6c359de0b698c0c76a298b1224e620046457b
thech <thech@LAPTOP-6F5NEE29.domain.actdsltmp>
2018-06-04 16:23:36 -0500

    initial commit

---
+++ lib/morena 7.1.31/examples/MorenaStudioLauncher.java
@@ -0,0 +1,9 @@
+
+public class MorenaStudioLauncher {
+
+	public static void main(String[] args) {
+		System.err.println("launching MorenaStudio ...");
+		MorenaStudio.main(args);
+	}
+
+}
---
+++ src/main.java
@@ -0,0 +1,12 @@
+import eu.gnome.morena.Camera;
+
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	}
+
+}
